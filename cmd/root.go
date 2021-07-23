fc83b23db2aa99c9ef66a3f7cc2a7f16a8bb5fce
sanjay tiwari <sanjay.tiwari@blackrock.com>
2021-07-23 13:18:10 -0400

    Initial Commit

---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "godemo",
+	Long:  "Add long description here. This is godemo for cli",
+	Short: "Add short description here",
+	Run:   func(cmd *cobra.Command, args []string) { fmt.Println("Hello CLI") },
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+func init() {
+	fmt.Println("inside init")
+	cobra.OnInitialize(initConfig)
+}
+func initConfig() {
+	fmt.Println("inside initConfig")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"godemo/cmd"
+)
+
+func main() {
+	fmt.Println("inside main")
+	cmd.Execute()
+
+}
